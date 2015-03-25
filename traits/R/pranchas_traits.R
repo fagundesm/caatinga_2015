e68969b8560235d78e4d8b10023fb7824afacf43
mfagundes <marvefa@gmail.com>
2015-03-25 13:11:22 -0300

    analises traits

---
+++ traits/R/pranchas_traits.R
@@ -0,0 +1,175 @@
+source("traits/R/Traits_caatinga.R")
+head(traits)
+####################################################################################################
+##############################                 Correlações                  ###########################
+#########################r###########################################################################
+
+all<-with(traits, cor(cbind(cap_arm_mad, dens_mad, efet_agua_mad,
+                           efet_agua_casca, cap_arm_casca, H_arvore_m, 
+                           diam_medio, thick_casca, cont_agua_mad, cont_agua_casca)))
+
+g8t <-with(traits, cor.test(cont_agua_mad, dens_mad))
+g8 <- ggplot(traits,aes(y=cont_agua_mad, x=dens_mad)) +
+  ylab("Wood Water content [%]")  +  xlab("Wood Density [g/cm³]") + 
+  theme(axis.title.y = element_text(size = rel(0.8), angle = 90), 
+  axis.title.x = element_text(size = rel(0.8)))+
+  geom_point()+
+  geom_smooth(method=lm)
+
+grt <-with(traits, cor.test(cont_agua_casca, dens_casca))
+gr <- ggplot(traits,aes(y=cont_agua_casca, x=dens_casca)) +
+  ylab("Bark Water content [%]")  +  xlab("Bark Density [g/cm³]") + 
+  theme(axis.title.y = element_text(size = rel(0.8), angle = 90), 
+  axis.title.x = element_text(size = rel(0.8)))+
+  geom_point()+
+  geom_smooth(method=lm)
+
+g7t <- with(traits, cor.test(thick_casca, dens_mad))
+g7 <- ggplot(traits,aes( y=thick_casca, x=dens_mad))  +
+  ylab("Bark thickness [mm]") + xlab("Wood density [g/cm³]") +
+  theme(axis.title.y = element_text(size = rel(0.8), angle = 90), 
+  axis.title.x = element_text(size = rel(0.8)))+
+  geom_point()+ 
+  geom_smooth(method=lm)
+
+gxt <-with(traits, cor.test(cont_agua_casca,thick_casca))
+gx <- ggplot(traits,aes(y=thick_casca, x=cont_agua_casca)) +
+  ylab("Bark thickness [mm]") + xlab("Wood Water Content [%]")  +
+  theme(axis.title.y = element_text(size = rel(0.8), angle = 90), 
+  axis.title.x = element_text(size = rel(0.8)))+
+  geom_point()+
+  geom_smooth(method=lm)
+
+g9t <-with(traits, cor.test(cont_agua_casca, dens_mad))
+g9 <- ggplot(traits,aes(y=cont_agua_casca, x=dens_mad))+
+  ylab("Bark water content [%]")  + xlab("Wood Density [g/cm³] ") +
+  theme(axis.title.y = element_text(size = rel(0.8), angle = 90), 
+  axis.title.x = element_text(size = rel(0.8)))+
+  geom_point()+
+  geom_smooth(method=lm)
+
+g10t <-with(traits, cor.test(cont_agua_casca, cont_agua_mad))
+g10 <- ggplot(traits,aes(y=cont_agua_casca, x=cont_agua_mad))  +
+  ylab("Bark water content [%]") + xlab("Wood Water Content [%]") +
+  theme(axis.title.y = element_text(size = rel(0.8), angle = 90), 
+  axis.title.x = element_text(size = rel(0.8)))+
+  geom_point()+
+  geom_smooth(method=lm)
+
+
+sheetcor <- grid.arrange(g8,gr, g7,gx,g9,g10, ncol=2)
+
+####################################################################################################
+#######################################      Gráficos     ##########################################
+####################################################################################################
+g3 <- ggplot(traits,aes(y=dens_mad, x=especie))+
+  xlab("") + ylab("Wood Density [g/cm³]") +
+  theme(axis.text.x = element_blank(),
+        axis.title.y = element_text(size = rel(0.6), angle = 90))+
+  guides(fill=FALSE)+
+  geom_boxplot(aes(fill=factor(traits$especie)))
+  #g3 <- g3 + theme(plot.margin = unit(c(1,1,-1, 1),"cm"))
+
+g6 <- ggplot(traits,aes(y=dens_casca, x= especie))+
+  xlab("") + ylab("Bark Density [g/cm³]") +
+  theme(axis.text.x = element_blank(),
+  axis.title.y = element_text(size = rel(0.6), angle = 90))+
+  guides(fill=FALSE)+
+  geom_boxplot(aes(fill=factor(traits$especie)))
+  #g6 <- g6 + theme(plot.margin = unit(c(1,1,-1, 1),"cm"))
+
+g2 <- ggplot(traits,aes(y=cont_agua_mad, x= especie))+
+   ylab("Wood Water content [%]") + xlab("") +
+  theme(axis.text.x = element_blank(),
+  axis.title.y = element_text(size = rel(0.6), angle = 90))+
+  guides(fill=FALSE)+
+  geom_boxplot(aes(fill=factor(traits$especie)))
+  #g2 <- g2 + theme(plot.margin = unit(c(1,1,-1, 1),"cm"))
+
+g5 <- ggplot(traits,aes(y=cont_agua_casca, x=especie))+
+   ylab("Bark Water Content[%]") + xlab("Specie")  +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1,size = 10),
+  axis.title.y = element_text(size = rel(0.6), angle = 90), 
+  axis.title.x = element_text(size = rel(1.0)))+
+  guides(fill=FALSE)+
+  geom_boxplot(aes(fill=factor(traits$spp)))
+  #g5 <- g5 + theme(plot.margin = unit(c(1,1,-1, 1),"cm"))
+
+sheetmad <- grid.arrange(g3,g6, g2,g5,g1,g4, ncol=3)
+
+g1 <- ggplot(traits,aes(  y=cap_arm_mad, x= especie))+
+  ylab("Wood Storage Capacity[%]") + xlab ("")+
+  theme(axis.text.x = element_blank(),
+  axis.title.y = element_text(size = rel(0.6), angle = 90))+
+  guides(fill=FALSE)
++  geom_boxplot(aes(fill=factor(traits$especie)))
+  #g1 <- g1 + theme(plot.margin = unit(c(1,1,-1, 1),"cm"))
+
+g4 <- ggplot(traits,aes(y=cap_arm_casca, x=especie))+
+  ylab("Bark Storage Capacity [%]") + xlab("") + 
+  theme(axis.text.x = element_blank(),
+  axis.title.y = element_text(size = rel(0.6), angle = 90))+
+  guides(fill=FALSE)+
+  geom_boxplot(aes(fill=factor(traits$especie)))
+  #g4 <- g4 + theme(plot.margin = unit(c(1,1,-1, 1),"cm"))
+
+g13 <- ggplot(traits,aes(y=diam_copa, x=especie))+
+  xlab("") + ylab("Canopy diameter [m]") +
+  theme(axis.text.x = element_blank(),
+  axis.title.y = element_text(size = rel(0.6), angle = 90))+
+  guides(fill=FALSE)+
+  geom_boxplot(aes(fill=factor(traits$especie)))
+  #g13 <- g13 + theme(plot.margin = unit(c(1,1,-1, 1),"cm"))
+
+g14 <- ggplot(traits,aes(y=H_arvore_m, x=especie))+
+  xlab("Species") + ylab("Tree Height [m]") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1,size = 10),
+  axis.title.y = element_text(size = rel(0.6), angle = 90), 
+  axis.title.x = element_text(size = rel(1.0)))+
+  guides(fill=FALSE)+
+  geom_boxplot(aes(fill=factor(traits$especie)))
+  #g14 <- g14 + theme(plot.margin = unit(c(1,1,-1, 1),"cm"))
+
+sheetcanopy <- grid.arrange(g13, g14, ncol=1)
+
+####################################################################################################
+#######################################      histogramas    ##########################################
+####################################################################################################
+media <- traits.mean
+
+dm  <- ggplot(media,aes (x= Mdens_mad))+
+  xlab("Wood density [g/cm³]") + ylab("Frequency") +
+  scale_x_continuous(breaks=seq(0.2, 1, 0.1))+
+  geom_histogram(colour = "black", fill = "grey", binwidth = 0.05)
+
+dc  <- ggplot(media,aes (x= Mdens_casca))+
+  xlab("Bark density [g/cm³]") + ylab("Frequency") +
+  scale_x_continuous(breaks=seq(0.2, 1, 0.1))+
+  geom_histogram(colour = "black", fill = "grey", binwidth = 0.05)
+  
+cam <- ggplot(media,aes (x= Mcap_arm_mad))+
+  xlab("Wood storage capacity") + ylab("Frequency") +
+  scale_x_continuous(breaks=seq(0.2, 2.5, 0.5))+
+  geom_histogram(colour = "black", fill = "grey", binwidth = 0.08) 
+
+cac <- ggplot(media,aes (x= Mcap_arm_casca))+
+  xlab("Bark storage capacity") + ylab("Frequency") +
+  scale_x_continuous(breaks=seq(1, 3.5, 0.5))+
+  geom_histogram(colour = "black", fill = "grey", binwidth = 0.08) 
+
+coam<- ggplot(media,aes (x= Mcont_agua_mad))+
+  xlab("Wood water content") + ylab("Frequency") +
+  scale_x_continuous(breaks=seq(0.2, 2, 0.5))+
+  geom_histogram(colour = "black", fill = "grey", binwidth = 0.08) 
+  
+coac<- ggplot(media,aes (x= Mcont_agua_casca))+
+  xlab("Bark water content") + ylab("Frequency") +
+  scale_x_continuous(breaks=seq(0.2, 3, 0.5))+
+  geom_histogram(colour = "black", fill = "grey", binwidth = 0.08)
+ 
+tc  <-  ggplot(media,aes (x= Mthick_casca))+
+  xlab("Bark thickness") + ylab("Frequency") +
+  scale_x_continuous(breaks=seq(0.2, 4.5, 0.8))+
+  geom_histogram(colour = "black", fill = "grey", binwidth = 0.1)
+  
+sheethist<- grid.arrange (dm,dc,cam,cac,coam,coac,tc, ncol=2)
---
+++ traits/R/reg_mult.R
@@ -0,0 +1,149 @@
+source("traits/R/Traits_caatinga.R")
+source ("experimento/R/rii.R")
+
+################################ ARRUMANDO TABELAS PARA ANÁLISE #################################################
+#Reshepando tabela de rii para poder fazer a regressão
+f <- rii_mt[1:4] #Selecionando somente o rri das folhas e variaveis categóricas
+fmelt <- dcast(f, plot + espnurse ~ target, value.var = "fol.mt.rii")
+fmelt<-arrange(fmelt, espnurse)
+
+#pegando somente as 3 réplicas que existem pra madeira também (em "f" econtram-se dados para as 5 réplicas)
+folhas.rii <- fmelt[c(1,4,5,7,9,10,13,14,15,17,19,20,21,22,24,27,28,30,31,34,35,36,38,40,41,42,43,46,48,49,51,54,55,
+             57,58,59,61,62,64,66,67,69,72,73,74,76,77,79,81,82,84,86,88,89,93,94,95,96,97,99),]
+
+traits$ID==copa$ID
+traits$especie==copa$especie
+
+
+#juntando tabelas de RII com traits
+reg <- cbind(traits, folhas.rii[,-c(1:2)])
+head(reg)
+
+###################################################|*|###########################################################
+
+# Regressão múltipla de cada target ~ traits com réplica (3) , sem interações 
+#regressão
+angico <-lm(angico ~ H_arvore_m + H_copa_m + diam_medio+  dens_mad+ 
+              cap_arm_mad+ efet_agua_mad + efet_agua_casca + thick_casca, reg)
+anova(angico)
+summary(angico)
+
+aroeira <-lm(aroeira ~ H_arvore_m + H_copa_m + diam_medio+  dens_mad+ 
+               cap_arm_mad+ efet_agua_mad + efet_agua_casca + thick_casca, reg)
+
+anova(aroeira)
+summary(aroeira)
+
+
+cating <-lm(cating~H_arvore_m + H_copa_m + diam_medio+  dens_mad+ 
+              cap_arm_mad+ efet_agua_mad + efet_agua_casca + thick_casca, reg)
+anova(cating)   
+summary(cating)
+
+######### teste de normalidade de resíduos / assumptions of lm # OK
+plot(angico)   
+plot(aroeira)
+plot(cating)
+hist(cating$residuals)
+hist(angico$residuals)
+hist(aroeira$residuals)
+shapiro.test(cating$residuals)
+shapiro.test(angico$residuals)
+shapiro.test(aroeira$residuals)
+
+############################################################################################33
+
+with(dados, cor(cbind(angico,cap_arm_mad, dens_mad, efet_agua_mad,
+                      efet_agua_casca, cap_arm_casca)))
+
+with(dados, cor(cbind(cating,cap_arm_mad,H_copa,diam_copa, dens_mad, 
+                      efet_agua_mad, efet_agua_casca, thick_casca,cap_arm_casca)))
+
+with(dados, cor(cbind(aroeira,cap_arm_mad,H_copa_m,diam_copa, dens_mad, 
+                      efet_agua_mad, efet_agua_casca, thick_casca,cap_arm_casca)))[,1]
+
+plot( angico~ efet_agua_casca, dados)
+plot( cating~ efet_agua_casca, dados)
+plot( aroeira~ efet_agua_casca, dados)
+plot( aroeira~ efet_agua_mad, dados)
+
+
+
+
+
+#Regressão multipla com réplica de trais e média de nurse# 18/02/15
+##depois da reunião com a GIs, ela pediu pra eu fazer a média de facilitação POR NURSE (das 3 targets) e utilizar
+#o modelo cheio de todos os traits importantes que são: 
+#H_arvore, diam_copa, dens_mad, dens_casca, cap_arm_mad, cap_arm_casca, efet_agua_mad, efet_agua_casca, thick_casca
+
+#PARA FAZER ESTA ANÁLISE EU TENHO QUE JUNTAS AS MÉDIAS DA TABELA TRAITS.MED COM OS DADOS rii.mean
+
+reg0 <-lm() ~  H_arvore + H_copa + diam_copa+  dens_mad+ 
+            cap_arm_mad+ efet_agua_mad + efet_agua_casca + thick_casca, dados)
+anova(reg0)
+summary(reg0)
+#Seleção de modelos
+reg1<- update(reg0, . ~ . - diam_copa)
+anova(reg1)
+summary(reg1)
+
+anova(reg0,reg1) #se não for significativo eu posso retirar as interações do modelo, se for eu tenho que deixa-las
+
+reg2 <- update(reg1, . ~. - H_arvore:diam_copa:cap_arm_mad:efet_agua_mad)
+anova(reg2)
+summary(reg2)
+
+anova(reg1,reg2)
+
+reg3 <- update(reg2, . ~.- H_arvore:diam_copa:dens_mad:cap_arm_mad )
+anova(reg3)                           
+summary(reg3)
+anova(reg2, reg3)
+
+reg4 <- update(reg3, ~. - dens_mad:cap_arm_mad:efet_agua_mad)
+anova(reg4)
+summary(reg4)
+anova(reg3,reg4)
+
+reg5 <- update (reg4, ~. - diam_copa:dens_mad:efet_agua_mad)
+anova(reg5)
+summary(reg5)
+anova(reg4,reg5)
+
+reg6 <- update(reg5, ~. - H_arvore:diam_copa:cap_arm_mad)
+anova(reg6)
+summary(reg6)
+anova(reg5,reg6)
+
+reg7<-  update(reg6,~. - diam_copa:dens_mad)
+anova(reg7)
+summary(reg7)
+
+reg8 <- update(reg7, ~. - dens_mad:cap_arm_mad ) #MINIMOMODELOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+anova(reg8)
+summary(reg8)
+
+#Regressão COM A MÉDIA DOS TRAITS e réplica por target# PRIMEIRA TENTATIVA
+# Fiz uma matriz de correlação com todas as variáveis,Estou excluindo as variáveis que são altamente correlacionadas (<0.6)
+# Restaram para eu utilizar os traits: " Mdens_mad,Mefet_agua_mad,MH_arvore,Mdiam_copa "
+media <- read.csv("data/traits_media.csv") 
+head(media)
+str(media)
+names(media)
+mat.cor <- cor(media[,c(14,20,22,23)])
+media_reg_mult <- select(media,rii,Mdens_mad,Mefet_agua_mad,MH_arvore,Mdiam_copa) #tabela com somente os traits para a regressão
+# Regressão múltipla
+mod0 <- lm(rii~Mdens_mad*Mefet_agua_mad*Mdiam_copa*MH_arvore, media_reg_mult)
+anova(mod0)
+summary (mod0)
+#seleçao dde modelos
+mod1 <- update(mod0, . ~ . - Mdens_mad:Mefet_agua_mad:Mdiam_copa:MH_arvore)
+anova(mod1)
+summary (mod1)
+mod2<- update(mod1, .~. - Mefet_agua_mad:Mdiam_copa:MH_arvore)
+anova(mod2)
+summary(mod2)
+mod3<- update(mod2, .~. - Mdens_mad:Mefet_agua_mad:Mdiam_copa)
+anova(mod3)
+
+
