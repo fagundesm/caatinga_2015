2e12f1dd6430250d236422eecd530222c997da75
mfagundes <marvefa@gmail.com>
2015-04-13 10:46:12 -0300

    erro sterr

--- experimento/R/barplotnurses.R
+++ experimento/R/barplotnurses.R
@@ -13,34 +13,29 @@ rii.tab <- summarise(group_by(rii.complete, espnurse, target),
           fol.sd.rii=sd(medt),
           se.fol.rii=stderr(medt))
 
-#Plotando facilitação para folhas por nurse
-#sci<- c("C.vit", "A.col","S.mac", "P.mon", "P.gar", "A.cea", "E.num", "C.has", "P.mar", "C.lep",
-#          "H.imp","P.sti","L.fer","M.ten","C.bla", "C.lep","B.che","C. glaz", "A.pyr", "S.tub")
-
-rii.tab$espnurse <-factor(rii.tab$espnurse,
-                           levels= c("burra","algodao", "angico", "catanduva", "catingueira",
-                                  "favelinha","imbiratanha","imburana","juca","mororo","paubranco",
-                                  "jubranca","jupreta","marmeleiro","cumaru","feijao",
-                                  "ipe","mofumbo","pereiro","umbu"))  
-
-
-#(ORDENAR POR COMPETIDORAS SPCSPECIFICO E FACILITADORAS)
+#Plotando facilitação para folhas por nurse, ordem das barras manual
+  sci<- c("S.macr","A.colu", "C. glaz","P.moni", "P.gard",
+          "E.numu", "P.marg", "L.ferr","B.chei", "P.stip",
+          "C.lepr","C.viti","M.tenu","C.lept","C.blan", 
+          "A.cear","C.hast","A.pyri", "S.tube","H.impe")
 
 bar<-  ggplot(rii.tab,aes(y=fol.nt.rii,x=espnurse, fill=factor(target)))+
        scale_fill_grey(start = 0.3, end = .8, name="Target", labels=c("A.col","M.uru","P.gar"))+
-       theme(axis.text.x = element_text(angle = 90, hjust = 1, size=15))+
+       theme(axis.text.x = element_text(angle = 90, hjust = 1, size=15),
+             panel.background = element_rect(fill='white', colour='black'), 
+             axis.text=element_text(colour="black"),
+             legend.text=element_text(size=15))+
        ylab("Interaction Index [RII]")  +  xlab("Nurse species")+ 
          geom_bar(stat="identity",position="dodge")+
-         geom_errorbar(aes(ymin=fol.nt.rii - se.fol.rii, ymax=fol.nt.rii+ se.fol.rii), width=.3,
+         geom_errorbar(aes(ymin=fol.nt.rii - se.fol.rii, ymax= fol.nt.rii+ se.fol.rii), width=.3,
          position=position_dodge(.9))+
-    scale_x_discrete(limits=c ("burra","algodao", "angico", "catanduva", "catingueira",
-  "favelinha","imbiratanha","imburana","juca","mororo","paubranco",
-"jubranca","mofumbo","jupreta","marmeleiro","cumaru","feijao",
-"ipe","pereiro","umbu"))+#,labels = sci)+
-         scale_y_continuous(limits=c(-0.5,0.5))+
-          theme(panel.background = element_rect(fill='white', colour='black'), 
-            axis.text=element_text(colour="black"),
-            legend.text=element_text(size=15))
+    scale_x_discrete(limits=c ("burra", "angico","paubranco", "catanduva", "catingueira",
+                               "favelinha","imbiratanha","juca","mororo","jubranca",
+                               "mofumbo","algodao","jupreta","imburana","marmeleiro",
+                               "cumaru","feijao","pereiro","umbu","ipe"),labels = sci)+
+         scale_y_continuous(limits=c(-0.5,0.5))
+
+
 
 
         
